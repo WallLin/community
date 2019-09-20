8de4513ccc5ba68a9877e6387290d6e3bc4f7d0b
kyrie <dinglin_sc@qq.com>
2019-09-20 10:01:17 +0800

    extract config from code to yml

--- src/main/java/com/kyrie/community/controller/AuthorizeController.java
+++ src/main/java/com/kyrie/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.kyrie.community.dto.AccessTokenDTO;
 import com.kyrie.community.dto.UserDTO;
 import com.kyrie.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,16 +20,23 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                             @RequestParam(name = "state") String state
                             ) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("c047774e5c90ab0d8ce3");
-        accessTokenDTO.setClient_secret("78def34915b4cdd5644a9df561b787eefdd3827b");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         UserDTO userDTO = githubProvider.getUserDTO(accessToken);
         System.out.println(userDTO.getName());
